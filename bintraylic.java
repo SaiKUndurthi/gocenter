10618cdc39501bacc711d9e90f22796936820eb4
Mark Galpin <markg@jfrog.com>
2019-01-27 12:06:32 -0800

    Initial Commit

---
+++ bintraylic.java
@@ -0,0 +1,590 @@
+{
+	"longName" : "The Academic Free License 3.0",
+	"name" : "AFL-3.0",
+	"url" : "http://www.opensource.org/licenses/afl-3.0.php"
+}
+{
+	"longName" : "Adaptive Public License 1.0",
+	"name" : "APL-1.0",
+	"url" : "http://www.opensource.org/licenses/apl1.0.php"
+}
+{
+	"longName" : "The Apache Software License, Version 2.0",
+	"name" : "Apache-2.0",
+	"url" : "http://www.opensource.org/licenses/apache2.0.php"
+}
+{
+	"longName" : "The Apache Software License, Version 1.0",
+	"name" : "Apache-1.0",
+	"url" : "http://apache.org/licenses/LICENSE-1.0"
+}
+{
+	"longName" : "The Attribution Assurance License",
+	"name" : "Attribution",
+	"url" : "http://www.opensource.org/licenses/attribution.php"
+}
+{
+	"longName" : "Boost Software License 1.0 (BSL1.0)",
+	"name" : "BSL-1.0",
+	"url" : "http://www.opensource.org/licenses/bsl1.0.html"
+}
+{
+	"longName" : "Computer Associates Trusted Open Source License 1.1",
+	"name" : "CA-TOSL-1.1",
+	"url" : "http://www.opensource.org/licenses/ca-tosl1.1.php"
+}
+{
+	"longName" : "Common Development and Distribution License (CDDL) 1.0",
+	"name" : "CDDL-1.0",
+	"url" : "http://www.opensource.org/licenses/cddl1.php"
+}
+{
+	"longName" : "Common Public Attribution License Version 1.0 (CPAL)",
+	"name" : "CPAL-1.0",
+	"url" : "http://www.opensource.org/licenses/cpal_1.0"
+}
+{
+	"longName" : "Day Spec License",
+	"name" : "Day",
+	"url" : "http://www.day.com/dam/day/downloads/jsr283/day-spec-license.htm"
+}
+{
+	"longName" : "Day Specification License Addendum",
+	"name" : "Day-Addendum",
+	"url" : "http://www.day.com/content/dam/day/downloads/jsr283/LICENSE.txt"
+}
+{
+	"longName" : "Bouncy Castle License",
+	"name" : "Bouncy-Castle",
+	"url" : "http://www.bouncycastle.org/licence.html"
+}
+{
+	"longName" : "EU DataGrid Software License",
+	"name" : "EUDATAGRID",
+	"url" : "http://www.opensource.org/licenses/eudatagrid.php"
+}
+{
+	"longName" : "Common Public License",
+	"name" : "CPL-1.0",
+	"url" : "http://www.opensource.org/licenses/cpl1.0.txt"
+}
+{
+	"longName" : "GNU Lesser General Public License, Version 2.1",
+	"name" : "LGPL-2.1",
+	"url" : "http://www.opensource.org/licenses/lgpl-2.1.php"
+}
+{
+	"longName" : "GNU Lesser General Public License, Version 3",
+	"name" : "LGPL-3.0",
+	"url" : "http://www.opensource.org/licenses/lgpl-3.0.html"
+}
+{
+	"longName" : "HSQLDB License",
+	"name" : "HSQLDB",
+	"url" : "http://hsqldb.org/web/hsqlLicense.html"
+}
+{
+	"longName" : "IBM Public License Version 1.0",
+	"name" : "IBMPL-1.0",
+	"url" : "http://www.opensource.org/licenses/ibmpl.php"
+}
+{
+	"longName" : "CeCILL B",
+	"name" : "CeCILL-B",
+	"url" : "http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html"
+}
+{
+	"longName" : "CeCILL C",
+	"name" : "CeCILL-C",
+	"url" : "http://www.cecill.info/licences/Licence_CeCILL-C_V1-en.html"
+}
+{
+	"longName" : "CeCILL v2",
+	"name" : "CeCILL-2",
+	"url" : "http://www.cecill.info/licences/Licence_CeCILL_V2-en.html"
+}
+{
+	"longName" : "CeCILL v2.1",
+	"name" : "CeCILL-2.1",
+	"url" : "http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html"
+}
+{
+	"longName" : "IPA Font License",
+	"name" : "IPAFont-1.0",
+	"url" : "http://www.opensource.org/licenses/ipafont.html"
+}
+{
+	"longName" : "ISC License (ISCL)",
+	"name" : "ISC",
+	"url" : "http://www.opensource.org/licenses/isc-license.txt"
+}
+{
+	"longName" : "Lucent Public License Version 1.02",
+	"name" : "Lucent-1.02",
+	"url" : "http://www.opensource.org/licenses/lucent1.02.php"
+}
+{
+	"longName" : "MirOS License",
+	"name" : "MirOS",
+	"url" : "http://www.opensource.org/licenses/miros.html"
+}
+{
+	"longName" : "Microsoft Public License",
+	"name" : "MS-PL",
+	"url" : "http://www.opensource.org/licenses/ms-pl.html"
+}
+{
+	"longName" : "JA-SIG",
+	"name" : "JA-SIG",
+	"url" : "http://www.jasig.org/cas/license"
+}
+{
+	"longName" : "Berkeley Software Distribution (BSD)",
+	"name" : "BSD",
+	"url" : "http://directory.fsf.org/wiki/License:BSD_4Clause"
+}
+{
+	"longName" : "The MIT License",
+	"name" : "MIT",
+	"url" : "http://www.opensource.org/licenses/mit-license.php"
+}
+{
+	"longName" : "The JSON License",
+	"name" : "JSON",
+	"url" : "http://www.json.org/license.html"
+}
+{
+	"longName" : "The MOTOSOTO OPEN SOURCE LICENSE - Version 0.9.1",
+	"name" : "Motosoto-0.9.1",
+	"url" : "http://www.opensource.org/licenses/motosoto.php"
+}
+{
+	"longName" : "Eclipse Public License 1.0",
+	"name" : "EPL-1.0",
+	"url" : "http://www.opensource.org/licenses/eclipse-1.0.php"
+}
+{
+	"longName" : "Educational Community License, Version 2.0",
+	"name" : "ECL2",
+	"url" : "http://www.opensource.org/licenses/ecl2.php"
+}
+{
+	"longName" : "Eiffel Forum License - Ver.2",
+	"name" : "Eiffel-2.0",
+	"url" : "http://www.opensource.org/licenses/ver2_eiffel.php"
+}
+{
+	"longName" : "Java HTML Tidy License",
+	"name" : "JTidy",
+	"url" : "http://jtidy.sourceforge.net/license.html"
+}
+{
+	"longName" : "The X.Net, Inc. License",
+	"name" : "Entessa-1.0",
+	"url" : "http://www.opensource.org/licenses/entessa.php"
+}
+{
+	"longName" : "Public Licence - EUPL v.1.1",
+	"name" : "EUPL-1.1",
+	"url" : "http://ec.europa.eu/idabc/eupl"
+}
+{
+	"longName" : "Fair License",
+	"name" : "Fair",
+	"url" : "http://www.opensource.org/licenses/fair.php"
+}
+{
+	"longName" : "Frameworx License 1.0",
+	"name" : "Frameworx-1.0",
+	"url" : "http://www.opensource.org/licenses/frameworx.php"
+}
+{
+	"longName" : "The GNU General Public License Version 2 with\n            Classpath Exception",
+	"name" : "GPL-2.0+CE",
+	"url" : "http://openjdk.java.net/legal/gplv2+ce.html"
+}
+{
+	"longName" : "Multics License",
+	"name" : "Multics",
+	"url" : "http://www.opensource.org/licenses/multics.txt"
+}
+{
+	"longName" : "NASA Open Source Agreement v1.3",
+	"name" : "NASA-1.3",
+	"url" : "http://www.opensource.org/licenses/nasa1.3.php"
+}
+{
+	"longName" : "NTP License",
+	"name" : "NTP",
+	"url" : "http://www.opensource.org/licenses/ntp-license.php"
+}
+{
+	"longName" : "NAUMEN Public License",
+	"name" : "NAUMEN",
+	"url" : "http://www.opensource.org/licenses/naumen.php"
+}
+{
+	"longName" : "The Nethack General Public License",
+	"name" : "Nethack",
+	"url" : "http://www.opensource.org/licenses/nethack.php"
+}
+{
+	"longName" : "Nokia Open Source License (NOKOS License) Version\n            1.0a",
+	"name" : "Nokia-1.0a",
+	"url" : "http://www.opensource.org/licenses/nokia.php"
+}
+{
+	"longName" : "The Non-Profit Open Software License version 3.0",
+	"name" : "NOSL-3.0",
+	"url" : "http://www.opensource.org/licenses/NOSL3.0.html"
+}
+{
+	"longName" : "The OCLC Research Public License 2.0 License",
+	"name" : "OCLC-2.0",
+	"url" : "http://www.opensource.org/licenses/oclc2.php"
+}
+{
+	"longName" : "SIL OPEN FONT LICENSE",
+	"name" : "Openfont-1.1",
+	"url" : "http://www.opensource.org/licenses/openfont.html"
+}
+{
+	"longName" : "The Open Group Test Suite License",
+	"name" : "Opengroup",
+	"url" : "http://www.opensource.org/licenses/opengroup.php"
+}
+{
+	"longName" : "The PHP License 3.0",
+	"name" : "PHP-3.0",
+	"url" : "http://www.opensource.org/licenses/php.php"
+}
+{
+	"longName" : "The PostgreSQL Licence",
+	"name" : "PostgreSQL",
+	"url" : "http://www.opensource.org/licenses/postgresql"
+}
+{
+	"longName" : "Public Domain",
+	"name" : "Public Domain",
+	"url" : "http://creativecommons.org/licenses/publicdomain/"
+}
+{
+	"longName" : "TECHNOLOGY LICENSE FROM SUN MICROSYSTEMS, INC.",
+	"name" : "Public Domain - SUN",
+	"url" : "http://gee.cs.oswego.edu/dl/classes/EDU/oswego/cs/dl/util/sun-u.c.license.pdf"
+}
+{
+	"longName" : "The Python License (CNRI Python License)",
+	"name" : "PythonPL",
+	"url" : "http://www.opensource.org/licenses/pythonpl.php"
+}
+{
+	"longName" : "Python Software Foundation License",
+	"name" : "PythonSoftFoundation",
+	"url" : "http://www.opensource.org/licenses/PythonSoftFoundation.php"
+}
+{
+	"longName" : "The Q Public License Version 1.0",
+	"name" : "QTPL-1.0",
+	"url" : "http://www.opensource.org/licenses/qtpl.php"
+}
+{
+	"longName" : "RealNetworks Public Source License Version 1.0",
+	"name" : "Real-1.0",
+	"url" : "http://www.opensource.org/licenses/real.php"
+}
+{
+	"longName" : "The Ricoh Source Code Public License",
+	"name" : "RicohPL",
+	"url" : "http://www.opensource.org/licenses/ricohpl.php"
+}
+{
+	"longName" : "Simple Public License (SimPL) 2.0",
+	"name" : "SimPL-2.0",
+	"url" : "http://www.opensource.org/licenses/simpl-2.0.html"
+}
+{
+	"longName" : "The Sleepycat License",
+	"name" : "Sleepycat",
+	"url" : "http://www.opensource.org/licenses/sleepycat.php"
+}
+{
+	"longName" : "The SUN PUBLIC LICENSE Version 1.0",
+	"name" : "SUNPublic-1.0",
+	"url" : "http://www.opensource.org/licenses/sunpublic.php"
+}
+{
+	"longName" : "Sybase Open Watcom Public License version 1.0",
+	"name" : "Sybase-1.0",
+	"url" : "http://www.opensource.org/licenses/sybase.php"
+}
+{
+	"longName" : "The University of Illinois/NCSA Open Source License",
+	"name" : "UoI-NCSA",
+	"url" : "http://www.opensource.org/licenses/UoI-NCSA.php"
+}
+{
+	"longName" : "Indiana University Extreme! Lab Software License",
+	"name" : "IU-Extreme-1.1.1",
+	"url" : "http://www.extreme.indiana.edu/viewcvs/~checkout~/XPP3/java/LICENSE.txt"
+}
+{
+	"longName" : "The Vovida Software License v. 1.0",
+	"name" : "VovidaPL-1.0",
+	"url" : "http://www.opensource.org/licenses/vovidapl.php"
+}
+{
+	"longName" : "The W3C SOFTWARE NOTICE AND LICENSE",
+	"name" : "W3C",
+	"url" : "http://www.opensource.org/licenses/W3C.php"
+}
+{
+	"longName" : "The wxWindows Library Licence",
+	"name" : "wxWindows",
+	"url" : "http://www.opensource.org/licenses/wxwindows.php"
+}
+{
+	"longName" : "The X.Net, Inc. License",
+	"name" : "Xnet",
+	"url" : "http://www.opensource.org/licenses/xnet.php"
+}
+{
+	"longName" : "The Zope Public License Ver.2.0",
+	"name" : "ZPL-2.0",
+	"url" : "http://www.opensource.org/licenses/zpl.php"
+}
+{
+	"longName" : "The zlib/libpng License",
+	"name" : "ZLIB",
+	"url" : "http://www.opensource.org/licenses/zlib-license.php"
+}
+{
+	"longName" : "Copyright 2002 (C) The Codehaus",
+	"name" : "Codehaus",
+	"url" : "http://classworlds.codehaus.org/license.html"
+}
+{
+	"longName" : "The TMate Open Source License",
+	"name" : "TMate",
+	"url" : "http://svnkit.com/license.html"
+}
+{
+	"longName" : "Do What the Fuck You Want to Public License",
+	"name" : "WTFPL",
+	"url" : "http://www.wtfpl.net"
+}
+{
+	"longName" : "The Code Project Open License 1.02",
+	"name" : "CPOL",
+	"url" : "http://www.codeproject.com/info/cpol10.aspx"
+}
+{
+	"longName" : "The Code Project Open License (CPOL) 1.02",
+	"name" : "CPOL-1.02",
+	"url" : "http://www.codeproject.com/info/cpol10.aspx"
+}
+{
+	"longName" : "Berkeley Software Distribution Simplified (BSD Simplified)",
+	"name" : "BSD 2-Clause",
+	"url" : "http://opensource.org/licenses/BSD-2-Clause"
+}
+{
+	"longName" : "Mozilla Public License Version 2.0",
+	"name" : "MPL-2.0",
+	"url" : "http://www.mozilla.org/MPL/2.0/"
+}
+{
+	"longName" : "The Go Programming Language",
+	"name" : "Go",
+	"url" : "http://golang.org/LICENSE"
+}
+{
+	"longName" : "Scala License",
+	"name" : "Scala",
+	"url" : "http://www.scala-lang.org/license.html"
+}
+{
+	"longName" : "Unlicense",
+	"name" : "Unlicense",
+	"url" : "http://unlicense.org/"
+}
+{
+	"longName" : "NUnit Test Adapter for Visual Studio 2.6.3",
+	"name" : "NUnit-Test-Adapter-2.6.3",
+	"url" : "http://nunit.org/index.php?p=vsTestAdapterLicense&r=2.6.3"
+}
+{
+	"longName" : "NUnit 2.6.3",
+	"name" : "NUnit-2.6.3",
+	"url" : "http://nunit.org/index.php?p=license&r=2.6.3"
+}
+{
+	"longName" : "Berkeley Software Distribution Simplified (BSD Simplified)",
+	"name" : "BSD Simplified",
+	"url" : "http://opensource.org/licenses/BSD-2-Clause"
+}
+{
+	"longName" : "Berkeley Software Distribution New (BSD New)",
+	"name" : "BSD New",
+	"url" : "http://opensource.org/licenses/BSD-3-Clause"
+}
+{
+	"longName" : "CC0 1.0 Universal",
+	"name" : "CC0-1.0",
+	"url" : "http://creativecommons.org/publicdomain/zero/1.0/"
+}
+{
+	"longName" : "Common Development and Distribution License (CDDL) 1.1",
+	"name" : "CDDL-1.1",
+	"url" : "https://glassfish.java.net/public/CDDL+GPL_1_1.html"
+}
+{
+	"longName" : "OpenSSL  LICENSE",
+	"name" : "OpenSSL",
+	"url" : "https://www.openssl.org/source/license.html"
+}
+{
+	"longName" : "GNU AFFERO GENERAL PUBLIC LICENSE v3",
+	"name" : "AGPL-V3",
+	"url" : "http://www.opensource.org/licenses/agpl-v3.html"
+}
+{
+	"longName" : "The Apache Software License, Version 1.1",
+	"name" : "Apache-1.1",
+	"url" : "http://apache.org/licenses/LICENSE-1.1"
+}
+{
+	"longName" : "The Apple Public Source License 2.0",
+	"name" : "APSL-2.0",
+	"url" : "http://www.opensource.org/licenses/apsl-2.0.php"
+}
+{
+	"longName" : "Artistic License 2.0",
+	"name" : "Artistic-License-2.0",
+	"url" : "http://www.opensource.org/licenses/artistic-license-2.0.php"
+}
+{
+	"longName" : "CUA Office Public License",
+	"name" : "CUAOFFICE-1.0",
+	"url" : "http://www.opensource.org/licenses/cuaoffice.php"
+}
+{
+	"longName" : "Historical Permission Notice and Disclaimer",
+	"name" : "Historical",
+	"url" : "http://www.opensource.org/licenses/historical.php"
+}
+{
+	"longName" : "Microsoft Reciprocal License",
+	"name" : "MS-RL",
+	"url" : "http://www.opensource.org/licenses/ms-rl.html"
+}
+{
+	"longName" : "The GNU General Public License Version 2",
+	"name" : "GPL-2.0",
+	"url" : "http://www.opensource.org/licenses/gpl-2.0.php"
+}
+{
+	"longName" : "GNU General Public License version 3",
+	"name" : "GPL-3.0",
+	"url" : "http://www.opensource.org/licenses/gpl-3.0.html"
+}
+{
+	"longName" : "Mozilla Public License 1.1 (MPL 1.1)",
+	"name" : "Mozilla-1.1",
+	"url" : "http://www.opensource.org/licenses/mozilla1.1.php"
+}
+{
+	"longName" : "The Open Software License 3.0",
+	"name" : "OSL-3.0",
+	"url" : "http://www.opensource.org/licenses/osl-3.0.php"
+}
+{
+	"longName" : "Reciprocal Public License 1.5",
+	"name" : "RPL-1.5",
+	"url" : "http://www.opensource.org/licenses/rpl1.5.txt"
+}
+{
+	"longName" : "Berkeley Software Distribution Simplified (BSD New)",
+	"name" : "BSD 3-Clause",
+	"url" : "http://opensource.org/licenses/BSD-3-Clause"
+}
+{
+	"longName" : "The OpenLDAP Public License",
+	"name" : "OpenLDAP",
+	"url" : "http://www.openldap.org/software/release/license.html"
+}
+{
+	"longName" : "Copyfree",
+	"name" : "Copyfree",
+	"url" : "http://copyfree.org/"
+}
+{
+	"longName" : "CeCILL v1",
+	"name" : "CeCILL-1",
+	"url" : "http://www.cecill.info/licences/Licence_CeCILL_V1.1-US.html"
+}
+{
+	"longName" : "Facebook Platform License",
+	"name" : "Facebook-Platform",
+	"url" : "https://github.com/facebook/facebook-android-sdk/blob/master/LICENSE.txt"
+}
+{
+	"longName" : "VIM License",
+	"name" : "VIM License",
+	"url" : "http://vimdoc.sourceforge.net/htmldoc/uganda.html#license"
+}
+{
+	"longName" : "LaTeX Project Public License v1.0",
+	"name" : "LPPL-1.0",
+	"url" : "https://spdx.org/licenses/LPPL-1.0.html"
+}
+{
+	"longName" : "Academic Free License v2.1",
+	"name" : "AFL-2.1",
+	"url" : "https://spdx.org/licenses/AFL-2.1.html"
+}
+{
+	"longName" : "libpng License",
+	"name" : "Libpng",
+	"url" : "https://spdx.org/licenses/Libpng.html"
+}
+{
+	"longName" : "Eclipse Public License 2.0",
+	"name" : "EPL-2.0",
+	"url" : "https://www.eclipse.org/legal/epl-2.0/"
+}
+{
+	"longName" : "Unicode License Agreement - Data Files and Software (2015)",
+	"name" : "Unicode-DFS-2015",
+	"url" : "https://spdx.org/licenses/Unicode-DFS-2015"
+}
+{
+	"longName" : "GNU Library General Public License v2 only",
+	"name" : "LGPL-2.0",
+	"url" : "https://spdx.org/licenses/LGPL-2.0-only.html"
+}
+{
+	"longName" : "European Union Public License 1.2",
+	"name" : "EUPL-1.2",
+	"url" : "https://spdx.org/licenses/EUPL-1.2.html"
+}
+{
+	"longName" : "Independent JPEG Group License",
+	"name" : "IJG",
+	"url" : "https://spdx.org/licenses/IJG.html"
+}
+{
+	"longName" : "University of Illinois/NCSA Open Source License",
+	"name" : "NCSA",
+	"url" : "https://spdx.org/licenses/NCSA.html"
+}
+{
+	"longName" : "Universal Permissive License v1.0",
+	"name" : "UPL-1.0",
+	"url" : "https://spdx.org/licenses/UPL-1.0.html"
+}
+{
+	"longName" : "ImageMagick License",
+	"name" : "ImageMagick",
+	"url" : "https://spdx.org/licenses/ImageMagick.html"
+}
---
+++ releases.md
@@ -0,0 +1,14 @@
+# Release Notes
+
+<!-- MarkdownTOC autolink="true" bracket="round" -->
+
+- [2018 December 10: GA](#2018-december-10-ga)
+
+<!-- /MarkdownTOC -->
+
+
+## 2018 December 10: GA
+* gocenter.jfrog.com goes live!
+* Basic ability to search for modules
+* Basic ability to request new modules be added to GoCenter
+* Basic ability to determine the status of a module/version
